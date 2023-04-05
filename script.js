3b1d2a489cc690466058425b93424d7a02934d40
Toomann9 <ntoomann9@gmail.com>
2023-04-05 11:03:36 +0000

    April 5 JS added

--- script.js
+++ script.js
@@ -1,122 +1,24 @@
-const myButtons = [
-    { label: " 1", onclick: () => console.log("Button 1 clicked") },
-    { label: " 2", onclick: () => console.log("Button 2 clicked") },
-    { label: " 3", onclick: () => console.log("Button 3 clicked") },
-    { label: " 4", onclick: () => console.log("Button 4 clicked") },
-    { label: " 5", onclick: () => console.log("Button 5 clicked") },
-    { label: " 6", onclick: () => console.log("Button 6 clicked") },
-    { label: " 7", onclick: () => console.log("Button 7 clicked") },
-    { label: " 8", onclick: () => console.log("Button 8 clicked") },
-    { label: " 9", onclick: () => console.log("Button 9 clicked") },
-    { label: " 10", onclick: () => console.log("Button 10 clicked") },
-    { label: " 11", onclick: () => console.log("Button 11 clicked") },
-    { label: " 12", onclick: () => console.log("Button 12 clicked") },
-    { label: " 13", onclick: () => console.log("Button 13 clicked") },
-    { label: " 14", onclick: () => console.log("Button 14 clicked") },
-    { label: " 15", onclick: () => console.log("Button 15 clicked") },
-    { label: " 16", onclick: () => console.log("Button 16 clicked") },
-    { label: " 17", onclick: () => console.log("Button 17 clicked") },
-    { label: " 18", onclick: () => console.log("Button 18 clicked") },
-    { label: " 19", onclick: () => console.log("Button 19 clicked") },
-    { label: " 20", onclick: () => console.log("Button 20 clicked") },
-    { label: " 21", onclick: () => console.log("Button 21 clicked") },
-    { label: " 22", onclick: () => console.log("Button 22 clicked") },
-    { label: " 23", onclick: () => console.log("Button 23 clicked") },
-    { label: " 24", onclick: () => console.log("Button 24 clicked") },
-    { label: " 25", onclick: () => console.log("Button 25 clicked") },
-    { label: " 26", onclick: () => console.log("Button 26 clicked") },
-    { label: " 27", onclick: () => console.log("Button 27 clicked") },
-    { label: " 28", onclick: () => console.log("Button 28 clicked") },
-    { label: " 29", onclick: () => console.log("Button 29 clicked") },
-    { label: " 30", onclick: () => console.log("Button 30 clicked") },
-    { label: " 31", onclick: () => console.log("Button 31 clicked") },
-    { label: " 32", onclick: () => console.log("Button 32 clicked") },
-    { label: " 33", onclick: () => console.log("Button 33 clicked") },
-    { label: " 34", onclick: () => console.log("Button 34 clicked") },
-    { label: " 35", onclick: () => console.log("Button 35 clicked") },
-    { label: " 36", onclick: () => console.log("Button 36 clicked") },
-    { label: " 37", onclick: () => console.log("Button 37 clicked") },
-    { label: " 38", onclick: () => console.log("Button 38 clicked") },
-    { label: " 39", onclick: () => console.log("Button 39 clicked") },
-    { label: " 40", onclick: () => console.log("Button 40 clicked") },
-    { label: " 41", onclick: () => console.log("Button 41 clicked") },
-    { label: " 42", onclick: () => console.log("Button 42 clicked") },
-    { label: " 43", onclick: () => console.log("Button 43 clicked") },
-    { label: " 44", onclick: () => console.log("Button 44 clicked") },
-    { label: " 45", onclick: () => console.log("Button 45 clicked") },
-    { label: " 46", onclick: () => console.log("Button 46 clicked") },
-    { label: " 47", onclick: () => console.log("Button 47 clicked") },
-    { label: " 48", onclick: () => console.log("Button 48 clicked") },
-    { label: " 49", onclick: () => console.log("Button 49 clicked") },
-    { label: " 50", onclick: () => console.log("Button 50 clicked") },
-    { label: " 51", onclick: () => console.log("Button 51 clicked") },
-    { label: " 52", onclick: () => console.log("Button 52 clicked") },
-    { label: " 53", onclick: () => console.log("Button 53 clicked") },
-    { label: " 54", onclick: () => console.log("Button 54 clicked") },
-    { label: " 55", onclick: () => console.log("Button 55 clicked") },
-    { label: " 56", onclick: () => console.log("Button 56 clicked") },
-    { label: " 57", onclick: () => console.log("Button 57 clicked") },
-    { label: " 58", onclick: () => console.log("Button 58 clicked") },
-    { label: " 59", onclick: () => console.log("Button 59 clicked") },
-    { label: " 60", onclick: () => console.log("Button 60 clicked") },
-    { label: " 61", onclick: () => console.log("Button 61 clicked") },
-    { label: " 62", onclick: () => console.log("Button 62 clicked") },
-    { label: " 63", onclick: () => console.log("Button 63 clicked") },
-    { label: " 64", onclick: () => console.log("Button 64 clicked") },
-    { label: " 65", onclick: () => console.log("Button 65 clicked") },
-    { label: " 66", onclick: () => console.log("Button 66 clicked") },
-    { label: " 67", onclick: () => console.log("Button 67 clicked") },
-    { label: " 68", onclick: () => console.log("Button 68 clicked") },
-    { label: " 69", onclick: () => console.log("Button 69 clicked") },
-    { label: " 70", onclick: () => console.log("Button 70 clicked") },
-    { label: " 71", onclick: () => console.log("Button 71 clicked") },
-    { label: " 72", onclick: () => console.log("Button 72 clicked") },
-    { label: " 73", onclick: () => console.log("Button 73 clicked") },
-    { label: " 74", onclick: () => console.log("Button 74 clicked") },
-    { label: " 75", onclick: () => console.log("Button 75 clicked") },
-    { label: " 76", onclick: () => console.log("Button 76 clicked") },
-    { label: " 77", onclick: () => console.log("Button 77 clicked") },
-    { label: " 78", onclick: () => console.log("Button 78 clicked") },
-    { label: " 79", onclick: () => console.log("Button 79 clicked") },
-    { label: " 80", onclick: () => console.log("Button 80 clicked") },
-    { label: " 81", onclick: () => console.log("Button 81 clicked") },
-    { label: " 82", onclick: () => console.log("Button 82 clicked") },
-    { label: " 83", onclick: () => console.log("Button 83 clicked") },
-    { label: " 84", onclick: () => console.log("Button 84 clicked") },
-    { label: " 85", onclick: () => console.log("Button 85 clicked") },
-    { label: " 86", onclick: () => console.log("Button 86 clicked") },
-    { label: " 87", onclick: () => console.log("Button 87 clicked") },
-    { label: " 88", onclick: () => console.log("Button 88 clicked") },
-    { label: " 89", onclick: () => console.log("Button 89 clicked") },
-    { label: " 90", onclick: () => console.log("Button 90 clicked") },
-    { label: " 91", onclick: () => console.log("Button 91 clicked") },
-    { label: " 92", onclick: () => console.log("Button 92 clicked") },
-    { label: " 93", onclick: () => console.log("Button 93 clicked") },
-    { label: " 94", onclick: () => console.log("Button 94 clicked") },
-    { label: " 95", onclick: () => console.log("Button 95 clicked") },
-    { label: " 96", onclick: () => console.log("Button 96 clicked") },
-    { label: " 97", onclick: () => console.log("Button 97 clicked") },
-    { label: " 98", onclick: () => console.log("Button 98 clicked") },
-    { label: " 99", onclick: () => console.log("Button 99 clicked") },
-    { label: " 100", onclick: () => console.log("Button 100 clicked") },
-    // add more buttons as needed
-  ];
-  
  
-  
-
 // Select the container element in which you want to insert the buttons
 const buttonContainer = document.getElementById("button-container");
-
-
-for (let i = 0; i < myButtons.length; i++) {
+ 
+for (let i =1; i<= 100; i++){ 
   const button = document.createElement("button");
-//   button = Math.floor(Math.random() ) + 1;
-  button.textContent = myButtons[i].label;
-  button.onclick = myButtons[i].onclick;
-  button.className = "round-button";
+   
+    button.innerHTML= Math.floor(Math.random()*100)+1;
+ 
+  button.className = "round-button"; 
   buttonContainer.appendChild(button);
-}
+
+ }
+   
+
+
+
+// for (let i = 0; i < myButtons.length; i++) {
+
+//   button.onclick = myButtons[i].onclick;
+// }
 
 function generateRandomNumber() {
     var randomNumber = Math.floor(Math.random() * 100);
