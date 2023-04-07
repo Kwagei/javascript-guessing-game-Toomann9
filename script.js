83ed60e5300943d8fc73b3805480d3c06c3145d3
Toomann9 <ntoomann9@gmail.com>
2023-04-07 10:35:31 +0000

    Completed

--- script.js
+++ script.js
@@ -1,100 +1,79 @@
- 
-// Select the container element in which you want to insert the buttons
-const buttonContainer = document.getElementById("button-container");
- 
-for (let i =1; i<= 100; i++){ 
-  const button = document.createElement("button");
-   
-    button.innerHTML= Math.floor(Math.random()*100)+1;
- 
-  button.className = "round-button"; 
-  buttonContainer.appendChild(button);
+let displayScreen;
+displayScreen = document.getElementById("result");
+displayScreen.style.fontSize = "20px";
+displayScreen.style.padding = "10px 6rem";
+// Generate an array of numbers from 1 to 100
+let numbers = [];
+for (let i = 1; i < 101; i++) {
+    numbers.push(i);
+}
 
- }
-   
+//  Fisher-Yates algorithm
+for (let i = numbers.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let temp = numbers[i];
+    numbers[i] = numbers[j];
+    numbers[j] = temp;
+}
 
+// Create the buttons in random order
+let buttonsDiv = document.getElementById("button-container");
+for (let i = 0; i < numbers.length; i++) {
+    let button = document.createElement("button");
+    button.className = "round-button";
+    button.id = numbers[i];
+    button.innerHTML = numbers[i];
+    buttonsDiv.appendChild(button);
 
+    // Add click event listener to each button
+    button.addEventListener("click", function() {
+        // Get the button id || the guessed number
+        let guessedNumber = parseInt(this.id);
 
-// for (let i = 0; i < myButtons.length; i++) {
+        // Check if the guessed number is correct
+        if (guessedNumber === randomNumber) {
+            document.getElementById("result").innerHTML = "Congratulations! You Won The Game!";
+            this.style.backgroundColor = "green";
+            document.getElementById("restart").style.display = "inline-block";
+        } else if (guessedNumber < randomNumber) {
+            document.getElementById("result").innerHTML = "Too low, try again.";
+            this.disabled = true;
+            this.style.backgroundColor = "black"; 
+            guessCounter--; // Decrement guess counter
+            document.getElementById("guesses").innerHTML = " " + guessCounter;
+            if (guessCounter === 0) {
+                document.getElementById("result").innerHTML = "Game Over!! The number was " + randomNumber + ".";
+                document.getElementById("restart").style.display = "inline-block"; 
+                for (let i = 0; i < numbers.length; i++) {
+                    document.getElementById(numbers[i]).disabled = true; // Disable all buttons
+                }
+            }
+        } else {
+            document.getElementById("result").innerHTML = "Too high, try again.";
+            this.disabled = true;
+            this.style.backgroundColor = "black"; 
+            guessCounter--; // Decrement guess counter
+            document.getElementById("guesses").innerHTML = " " + guessCounter;
+            if (guessCounter === 0) {
+                document.getElementById("result").innerHTML = "Game Over!! The number was " + randomNumber + ".";
+                document.getElementById("restart").style.display = "inline-block"; 
+                for (let i = 0; i < numbers.length; i++) {
+                    document.getElementById(numbers[i]).disabled = true; // Disable all buttons
+                }
+            }
+        }
+    });
+}
 
-//   button.onclick = myButtons[i].onclick;
-// }
+// Generate a random number for the user to guess
+let randomNumber = Math.floor(Math.random() * 100) + 1;
 
-function generateRandomNumber() {
-    var randomNumber = Math.floor(Math.random() * 100);
-  
-    return randomNumber + 1;
-  }
-  // Get a reference to the HTML element you want to set the value of
+// Initialize guess counter to 7
+let guessCounter = 7;
+document.getElementById("guesses").innerHTML = " " + guessCounter;
 
-// Generate a random number and set the value of the HTML element to it
-button.innerText = generateRandomNumber();
-
-  
-
-{/* <div id="random-number"></div>
-
-<script>
-  const randomElement = document.getElementById("random-number");
-  const randomNumber = Math.floor(Math.random() * 100) + 1;
-  randomElement.textContent = randomNumber;
-</script>
- */}
-
-
-
-
-
-// const buttonDiv = document.getElementsByClassName('button-wrapper');
-
-// const buttonBtn = document.createElement("button");
-
-// buttonBtn.innerText = "One 1";
-// buttonBtn.className = "round-button";
-
-// buttonDiv.appendChild(buttonBtn);
-// How do you insert multiple buttons in an html document using js
-
-// How do you insert numbers in a button in an html document using js
-
-// function getNum (){
-    // let space = [];
-
-    // for(let i = 1; i < 101; i++){
-        // space.push(i);
-    // } 3, 539, 000
-
-//     for (let i = 0; i < space.length; i++){
-//         const myButton = document.createElement('button');
-//             myButton.innerText = space[i];
-//         document.querySelector('.button-wrapper').appendChild(button);
-//     }
-
-    // const numbersDiv = document.querySelector('.button-wrapper');
-    // const randomNumbers = Array.from({length: numButtons}, () => Math.floor(Math.random() * 100));
-
-    // for (let i = 1; i <= 100; i++) {
-    // const numberSpan = document.createElement('button');
-    // numberSpan.innerHTML = i;
-    // numbersDiv.appendChild(numberSpan);
-// }
-// }
-
-
-
-
-
-
-
-// let keyboard = document.getElementsByClassName('button-wrapper');
-// let buttons = keyboard.getElementsByClassName('round-button');
-
-// {
-//     let theClickedItem = buttons[i].addEventListener('click', displayFn)
-// }
-
-// function displayFn(e){
-//     if (e.target.innerText === 7){
-//         confirm('Congratulations', 'play again ?');
-//     } 
-// }
+// Add click event listener to restart button
+document.getElementById("restart").addEventListener("click", function() {
+    // Reload the page to start a new game
+    location.reload();
+});
